ed04b5eafc25a6a61b0df30e95e5a1ad1a2bc2f1
MohammadFarhan2724 <anjumfarhan2724@gmail.com>
2026-07-19 10:30:22 +0530

    Called create_table() function, and created tables on neonDB

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from .db.database import create_table
+from .db import models
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "SlipSync backend is running"}
+
+create_table()
